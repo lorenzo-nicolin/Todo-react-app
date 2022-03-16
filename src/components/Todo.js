5c42c6c724e0243ed80396589f122c11161ebcda
Lorenzo <lorenzogovender152@gmail.com>
2022-03-16 18:48:10 +0200

    Completed todo app

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,28 +1,46 @@
-import React,{useState} from 'react'
-import TodoForm from './TodoForm'
-import TodoList from './TodoList';
-import { BiAbacus } from "react-icons/bi";
+import React, { useState } from 'react';
+import TodoForm from './TodoForm';
+import { RiCloseCircleLine } from 'react-icons/ri';
+import { TiEdit } from 'react-icons/ti';
 
-function Todo({todos, completeTodo}) {
-  
+const Todo = ({ todos, completeTodo, removeTodo, updateTodo }) => {
   const [edit, setEdit] = useState({
-    id:null,
-    value:''
+    id: null,
+    value: ''
   });
 
+  const submitUpdate = value => {
+    updateTodo(edit.id, value);
+    setEdit({
+      id: null,
+      value: ''
+    });
+  };
 
+  if (edit.id) {
+    return <TodoForm edit={edit} onSubmit={submitUpdate} />;
+  }
 
-  return todos.map((todo, index) => {
-    <div 
-    className={todo.isComplete ? 'todo-complete' : 'todo-row'} 
-    key={index}>
-        <div key={todo.id} onClick={() => completeTodo(todo.id)}>
-          {todo.text}
-          <BiAbacus />
+  return todos.map((todo, index) => (
+    <div
+      className={todo.isComplete ? 'todo-row complete' : 'todo-row'}
+      key={index}
+    >
+      <div key={todo.id} onClick={() => completeTodo(todo.id)}>
+        {todo.text}
+      </div>
+      <div className='icons'>
+        <RiCloseCircleLine
+          onClick={() => removeTodo(todo.id)}
+          className='delete-icon'
+        />
+        <TiEdit
+          onClick={() => setEdit({ id: todo.id, value: todo.text })}
+          className='edit-icon'
+        />
       </div>
     </div>
-  }) 
+  ));
+};
 
-}
-
-export default Todo
+export default Todo;

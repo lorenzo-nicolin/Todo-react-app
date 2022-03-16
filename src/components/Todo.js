3b8396c92184d78f2424f3aad3b1d849887a04f7
Lorenzo <lorenzogovender152@gmail.com>
2022-03-16 18:32:34 +0200

    new

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,9 +1,28 @@
 import React,{useState} from 'react'
+import TodoForm from './TodoForm'
+import TodoList from './TodoList';
+import { BiAbacus } from "react-icons/bi";
 
-function Todo() {
-  return (
-    <div>Todo</div>
-  )
+function Todo({todos, completeTodo}) {
+  
+  const [edit, setEdit] = useState({
+    id:null,
+    value:''
+  });
+
+
+
+  return todos.map((todo, index) => {
+    <div 
+    className={todo.isComplete ? 'todo-complete' : 'todo-row'} 
+    key={index}>
+        <div key={todo.id} onClick={() => completeTodo(todo.id)}>
+          {todo.text}
+          <BiAbacus />
+      </div>
+    </div>
+  }) 
+
 }
 
 export default Todo
--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import TodoForm from './TodoForm';
+import Todo from './Todo';
 
 function TodoList() {
     const [todos, setTodos] = useState([]);
@@ -14,6 +15,16 @@ function TodoList() {
       setTodos(newTodos);
       // console.log(...todos);
     };
+
+    const completeTodo = id => {
+        let updatedTodos = todos.map(todo => {
+           if(todo.id == id){
+             todo.isComplete = !todo.isComplete
+           }
+           return todo;
+        }) 
+        setTodos(updatedTodos);
+    }
   
 
   
@@ -25,6 +36,7 @@ function TodoList() {
       <>
         <h1>What's the Plan for Today?</h1>
         <TodoForm onSubmit={addTodo} />
+        <Todo todos={todos} completeTodo={completeTodo} />
       </>
     );
   }
